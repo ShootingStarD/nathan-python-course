0ae01b0fd125ff9ef2c4709e569d83f14fd3a937
Nathan Vaccaro <nac3pppoan@gmail.com>
2024-01-10 16:28:45 +0100

    changed .gitignore

--- tests_class_exceptions/src/subscriptions.py
+++ tests_class_exceptions/src/subscriptions.py
@@ -20,12 +20,8 @@ def compute_sncb_subscription(
     -------
     int
         the price of the SCNB subscription of a client based on its personal info
-    Raises
-    ------
-        if age < 62 and working status is retired
+
     """
-    if user.age < 62 and user.working_status == "retired":
-        raise ValueError("mytho")
     if user.age <= 24 and user.working_status == "student":
         return 50
     elif user.working_status == "retired":
